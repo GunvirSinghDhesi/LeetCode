9613699f0955242c91ddd127343b30b36d824300
GunvirSinghDhesi <137956552+GunvirSinghDhesi@users.noreply.github.com>
2025-04-12 22:19:47 -0400

    Time: 76 ms (93.30%) | Memory: 20.4 MB (81.07%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -3,9 +3,7 @@ class Solution:
         k = 2
 
         for i in range(2, len(nums)):
-            if nums[i] == nums[k - 2]:
-                pass
-            else:
+            if nums[i] != nums[k - 2]:
                 nums[k] = nums[i]
                 k += 1
         return k
