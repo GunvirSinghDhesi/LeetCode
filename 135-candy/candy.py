b341d23986975ff8f8b8285e8b5976f826bd2a45
GunvirSinghDhesi <137956552+GunvirSinghDhesi@users.noreply.github.com>
2025-04-15 11:49:07 -0400

    Time: 15 ms (68.98%) | Memory: 19.8 MB (91.93%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,17 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+
+        nArr=[1]*len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i-1]<ratings[i]:
+                nArr[i]+=nArr[i-1]
+
+     
+        for i in range(len(ratings)-1,0,-1):
+            if ratings[i-1]>ratings[i] and nArr[i-1]<=nArr[i]:
+                nArr[i-1]=nArr[i]+1
+
+
+
+        totalCandies = sum(nArr)
+        return totalCandies
