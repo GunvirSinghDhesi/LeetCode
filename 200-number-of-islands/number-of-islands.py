4253db8adcbf85d79a2f423f95f8689b84e3ddbe
GunvirSinghDhesi <137956552+GunvirSinghDhesi@users.noreply.github.com>
2025-04-28 11:11:33 -0400

    Time: 256 ms (46.48%) | Memory: 20.3 MB (58.37%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count=0
+        toExplore=[]
+        
+
+        def bfs(m,n):
+            if m>len(grid)-1 or n>len(grid[0])-1 or m<0 or n<0:
+                return 0
+            if grid[m][n]=="1":
+                grid[m][n] = "-1" 
+                bfs(m+1,n)
+                bfs(m,n+1)
+                bfs(m-1,n)
+                bfs(m,n-1)
+                return 1
+            
+
+        m=0
+        while m < len(grid):
+            n=0
+            while n < len(grid[0]):
+                # check if explored -1
+                if grid[m][n] == "1":
+                    grid[m][n] = "-1"
+                    bfs(m+1,n)
+                    bfs(m,n+1)
+                    count+=1
+                n+=1
+            m+=1
+        return count
