3e71e04db01347365351ad38cc0823fc30a44040
GunvirSinghDhesi <137956552+GunvirSinghDhesi@users.noreply.github.com>
2025-04-28 11:14:18 -0400

    Time: 251 ms (53.07%) | Memory: 20.1 MB (82.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,15 +5,15 @@ class Solution:
         
 
         def bfs(m,n):
-            if m>len(grid)-1 or n>len(grid[0])-1 or m<0 or n<0:
+            if m>len(grid)-1 or n>len(grid[0])-1 or m<0 or n<0 or grid[m][n]!="1":
                 return 0
-            if grid[m][n]=="1":
-                grid[m][n] = "-1" 
-                bfs(m+1,n)
-                bfs(m,n+1)
-                bfs(m-1,n)
-                bfs(m,n-1)
-                return 1
+            grid[m][n] = "-1" 
+            bfs(m+1,n)
+            bfs(m,n+1)
+            bfs(m-1,n)
+            bfs(m,n-1)
+            return 1
+
             
 
         m=0
